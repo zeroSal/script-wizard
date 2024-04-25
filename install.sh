5e08da5f65ccc4039c6d72635576a6d33832d124
Luca Saladino <sal65535@protonmail.com>
2024-04-25 16:25:11 +0200

    Implemented macos support

--- install.sh
+++ install.sh
@@ -19,13 +19,46 @@ By using this software you must agree all below terms:
 [?] If you agree, press [Enter] to procede:"
 read junk
 
-# TODO: Implement non-APT environment installation
-if [ $(dpkg-query -W -f='${Status}' python3 > /dev/null 2>&1 | grep -c "ok installed") -eq 0 ]; then
-	# Python3 not installed on the system, just install it!
-  echo "[*] Installing python interpreter on the system..."
-	apt update > /dev/null 2>&1;
-	apt install python3 > /dev/null 2>&1;
+if [ "$(uname)" == "Darwin" ]; then
+  install_python_mac()
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  install_python_linux()
+else
+  echo "[x] Only MacOS and Linux operating system are supported."
 fi
+
+install_python_mac () {
+  if ! command -v brew >/dev/null; then
+    echo "[x] Brew package manager not installed."
+  fi
+
+  if ! brew list -1 | grep -q "python@3"; then
+    echo "[*] Installing python interpreter on the system..."
+    if ! brew install -y "python3" > /dev/null 2>&1; then
+      echo "[x] Cannot install python via brew."
+    fi
+  fi
+}
+
+install_python_linux () {
+  if command -v apt-get >/dev/null; then
+    install_python_apt()
+  elif command -v yum >/dev/null; then
+    install_python_yum()
+  else
+    echo "[x] Neither apt not yum found as package manager."
+    exit
+  fi
+}
+
+install_python_apt () {
+  if [ $(dpkg-query -W -f='${Status}' python3 > /dev/null 2>&1 | grep -c "ok installed") -eq 0 ]; then
+    # Python3 not installed on the system, just install it!
+    echo "[*] Installing python interpreter on the system..."
+    apt update > /dev/null 2>&1;
+    apt install python3 > /dev/null 2>&1;
+  fi
+}
 
 # Install python requirements
 echo "[*] Installing python requirements..."
